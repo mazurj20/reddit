7832dc84feea1a28c5e9a3c45b48ea32d4e48a74
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-11-12 17:14:18 -0500

    fetch user id upon login/signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,39 @@
 import React from "react";
 import "../styles/Navbar.css";
 import RedditIcon from "@material-ui/icons/Reddit";
-import { SearchOutlined, MoreVert } from "@material-ui/icons";
+import {
+  SearchOutlined,
+  MoreVert,
+  AddIcCallRounded,
+  PersonAddDisabled,
+} from "@material-ui/icons";
 import { Avatar, IconButton } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { auth, provider } from "../firebase";
 import { actionTypes } from "../reducer";
 import { useStateValue } from "../stateprovider";
+import axios from "../axios";
 
 function Navbar() {
   const [{ user }, dispatch] = useStateValue();
 
-  const logIn = () => {
-    auth
+  const signUp = (email) => {
+    axios.post("/users", { email: email }).then((res) => console.log(res.data));
+  };
+
+  const findId = (email) => {
+    const header = { email: email };
+    axios.get("/login", { headers: header }).then((res) => {
+      if (res.data.length < 1) {
+        signUp(email);
+      } else {
+        console.log("yo");
+      }
+    });
+  };
+
+  const logIn = async () => {
+    await auth
       .signInWithPopup(provider)
       .then((res) => {
         dispatch({
@@ -20,7 +41,8 @@ function Navbar() {
           user: res.user,
         });
 
-        console.log(res);
+        console.log(res.user.email);
+        findId(res.user.email);
       })
       .catch((error) => alert(error.message));
   };
@@ -38,7 +60,7 @@ function Navbar() {
         <input placeholder="Search" style={{ flex: "1" }} type="text" />
       </div>
       <div className="Navbar_right">
-        {!user && <Button onClick={logIn}>log in</Button>}
+        {!user && <Button onClick={() => logIn()}>log in</Button>}
         <IconButton>
           <MoreVert />
         </IconButton>
