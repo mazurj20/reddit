84d6619df9847e3053eed61fd0e7176e53ddce34
tmurphy3 <tim.murphy@uconn.edu>
2020-11-19 17:15:01 -0500

    responsiveness

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -32,6 +32,7 @@ const Home = ({
   return (
     <>
       <Sidebar
+        className="Sidebar"
         setCreateSubredditForm={setCreateSubredditForm}
         setCreatePostForm={setCreatePostForm}
       />
